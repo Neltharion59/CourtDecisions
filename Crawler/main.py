e4cbec3799082f4466cc7d44a0595bd6b3e48288
Neltharion59 <lukawh5959@gmail.com>
2020-10-06 19:06:46 +0200

    Wait time randomization, occasional human-like break, log creation

--- Crawler/main.py
+++ Crawler/main.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+from random import randint
+from random import random
+from datetime import datetime
 import requests
-
 
 # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
 def readable_size(num, suffix='B'):
@@ -12,6 +14,21 @@ def readable_size(num, suffix='B'):
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
+def current_datetime():
+    now = datetime.now()
+    return now.strftime("%d-%m-%Y %H-%M-%S")
+
+
+start_datetime = current_datetime()
+
+
+def log_activity(message):
+    print(message)
+    log_file_object = open("./Crawler/Log/activity_log_" + start_datetime + ".txt", "a+")
+    log_file_object.write(message)
+    log_file_object.close()
 
 
 pager_link = "https://obcan.justice.sk/infosud?p_p_id=isufront_WAR_isufront&p_p_col_id=column-1&p_p_col_count=1&p_p_mode=view&_isufront_WAR_isufront_view=list&p_p_state=normal&_isufront_WAR_isufront_entityType=rozhodnutie&_isufront_WAR_isufront_cur="
@@ -34,12 +51,18 @@ while True:
     element_file_list_container = driver.find_element_by_id("_isufront_WAR_isufront_mapsSearchContainer")
     element_link_list = element_file_list_container.find_elements_by_tag_name("a")
 
+    if random() < 0.03:
+        break_time = randint(300, 600)
+        log_activity(current_datetime() + " Taking a human-like break for %d seconds\n" % break_time)
+        sleep(break_time)
+        log_activity(current_datetime() + " Back to work\n")
+
     for element in element_link_list:
-        sleep(5)
+        sleep(randint(5, 12))
         driver.execute_script("arguments[0].scrollIntoView();", element)
         ActionChains(driver).key_down(Keys.CONTROL).click(element).key_up(Keys.CONTROL).perform()
         driver.switch_to.window(driver.window_handles[1])
-        sleep(5)
+        sleep(randint(5, 12))
         element_download_button = driver.find_element_by_class_name("documentDownload")
 
         req = requests.get(element_download_button.get_attribute("href"), allow_redirects=True)
@@ -53,12 +76,12 @@ while True:
         with open("./Crawler/file_ids.txt", "a") as file_object:
             file_object.write(str(file_counter) + " " + file_name + "\n")
 
-        print("File count: " + str(file_counter) + "\nTotal size " + readable_size(total_file_size))
+        log_activity(current_datetime + "\nFile count: " + str(file_counter) + "\nTotal size " + readable_size(total_file_size))
 
-        sleep(30)
+        sleep(randint(22, 51))
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
 
-    sleep(10)
+    sleep(randint(7, 20))
     page_counter = page_counter + 1
     driver.get(pager_link + str(page_counter))
