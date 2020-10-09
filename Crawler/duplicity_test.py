b56a4f4aafb39b315cf963e0a741980afc3b9280
Neltharion59 <lukawh5959@gmail.com>
2020-10-09 22:29:04 +0200

    Mechanism to clean up duplicites from id list enhanced with deleting files from disk

--- Crawler/duplicity_test.py
+++ Crawler/duplicity_test.py
@@ -1,4 +1,8 @@
+from os import listdir, remove
+from os.path import isfile, join
+
 perform_remedy = True
+file_directory = "D:/Rozsudky"
 
 filename_dict = {}
 with open("./file_ids.txt", "r", encoding='UTF-8') as file_object:
@@ -19,29 +23,44 @@ for key in filename_dict:
 
 print(ids_to_delete)
 
+if not perform_remedy:
+    exit()
+
+if len(ids_to_delete) > 0:
+    with open("./file_ids.txt", "r", encoding='UTF-8') as file_object,\
+         open("./file_ids_temp.txt", "w+", encoding='UTF-8') as file_object_temp:
+        for line in file_object:
+            tokens = line.replace('\n', '').split(' ')
+            file_id = tokens[0]
+            if file_id not in ids_to_delete:
+                file_object_temp.write(line)
+
+    file_count = 0
+    with open("./file_ids.txt", "w+", encoding='UTF-8') as file_object,\
+         open("./file_ids_temp.txt", "r", encoding='UTF-8') as file_object_temp:
+        for line in file_object_temp:
+            file_object.write(line)
+            file_count = file_count + 1
+
+    with open("./count.txt", 'w+') as curr_file:
+        curr_file.write(str(file_count))
 
 with open("./file_ids_temp.txt", "w+", encoding='UTF-8') as file_object_temp:
     file_object_temp.write('')
-if not perform_remedy or len(ids_to_delete) == 0:
-    exit()
 
-with open("./file_ids.txt", "r", encoding='UTF-8') as file_object, open("./file_ids_temp.txt", "w+", encoding='UTF-8') as file_object_temp:
+good_id_list = []
+with open("./file_ids.txt", "r", encoding='UTF-8') as file_object:
     for line in file_object:
         tokens = line.replace('\n', '').split(' ')
         file_id = tokens[0]
-        if file_id not in ids_to_delete:
-            file_object_temp.write(line)
+        good_id_list.append(file_id)
 
-file_count = 0
-with open("./file_ids.txt", "w+", encoding='UTF-8') as file_object, open("./file_ids_temp.txt", "r", encoding='UTF-8') as file_object_temp:
-    for line in file_object_temp:
-        file_object.write(line)
-        file_count = file_count + 1
 
-with open("./count.txt", 'w+') as curr_file:
-    curr_file.write(str(file_count))
+all_id_list = [f.replace('.pdf', '') for f in listdir(file_directory) if isfile(join(file_directory, f))]
+bad_id_list = list(set(all_id_list) - set(good_id_list))
+for file_id in bad_id_list:
+    remove(file_directory + "/" + file_id + ".pdf")
 
-with open("./file_ids_temp.txt", "w+", encoding='UTF-8') as file_object_temp:
-    file_object_temp.write('')
+
 
 
